5d20fdc5688100ec4a822862ef8a80b69cca0c17
Sagar8989-Null <sagarjamadar8989@gmail.com>
2025-06-24 02:09:18 +0530

    Added routing and spin loader

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -1,14 +1,20 @@
 import React, { useState } from 'react'
 import Navbar from '../components/Navbar'
 import { useForm } from 'react-hook-form';
+// import { Link } from 'react-router-dom';
 import '../Styles/Home.css'
+import { useNavigate } from 'react-router-dom';
+import ClipLoader from 'react-spinners/ClipLoader';               // temporary usestate we won't be needing this further
 
 const Home = () => {
 
   const { register, handleSubmit } = useForm();
   const [responseText, setResponseText] = useState('');           // temporary usestate we won't be needing this further
+  const navigate = useNavigate();                                 // temporary usestate we won't be needing this further
+  const [loading, setLoading] = useState(false);                  // temporary usestate we won't be needing this further
 
   const onSubmit = async (data) => {
+    setLoading(true);
     try {
       const response = await fetch('http://localhost:3000/chat', {
         method: 'POST',
@@ -23,8 +29,11 @@ const Home = () => {
 
       setResponseText(result.reply);
 
+      navigate('/main', { state: { responseText: result.reply } })
+
     }
     catch (error) {
+      setLoading(false)
       console.error('API call failed', error);
     }
   }
@@ -35,17 +44,27 @@ const Home = () => {
       <div className="main">
         <form onSubmit={handleSubmit(onSubmit)}>
           <input type="text" placeholder="Prompt Here" {...register("userprompt")} />
-          <button type="submit">Submit</button>
+          {/* <Link to='/main'> */}
+          {/* <button type="submit"> Submit</button> */}
+          <button type="submit" disabled={loading}> Submit</button>
+          {/* </Link> */}
         </form>
       </div>
 
       {/* The below code temporary and for testing purpose */}
-      {responseText && (
-          <div className="response-output">
-            <h3>Generated Output:</h3>
-            <pre>{responseText}</pre>
-          </div>
-        )}
+
+      {loading?<div className="spinner-container">
+        <ClipLoader color="#36d7b7" size={40} />
+      </div>:<h2 className='spinner-container'> Ensure that your Backend is live</h2>}
+
+      {responseText &&(
+        <div className="response-output">
+          <h3>Generated Output:</h3>
+          <pre>{responseText}</pre>
+        </div>
+      )}
+
+      {/*  */}
     </div>
   )
 }
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,9 +2,24 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import {createBrowserRouter,RouterProvider,} from "react-router-dom";
+import Main from './Pages/Main.jsx';
+
+const router = createBrowserRouter([
+  {
+  path : "/",
+  element : <App/>
+  },
+  {
+    path: "/main",
+    element : <Main/>
+  }
+
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+      {/* <App /> */}
+      <RouterProvider router={router} />
   </StrictMode>,
 )
